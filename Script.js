72de92a3bf61f84120ae8bf966c80050d7859b83
wildercanteorellana <138070333+wildercanteorellana@users.noreply.github.com>
2023-06-28 16:44:54 -0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,16 @@
+var mamalona ="wilder";
+mamalona = 7;
+var firstname = "wilder";
+var lastname = "cante";
+var numberOfsiblings = 2;
+var hasPets = true;
+
+
+console.log(firstname);
+console.log(lastname);
+console.log(numberOfsiblings);
+console.log(hasPets);
+
+var button = document.getElementById("hi");
+
+button.addEventListener("click", function(){})
